c7d37cdbd95d57cd8c8d56e5ef30189dc1080824
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-02-01 20:12:32 +0100

    creada un servicio para obtener los productos por http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+// el app.module es global
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     SharedModule,
     CoreModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
