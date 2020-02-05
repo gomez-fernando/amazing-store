c456b98563536af860969fa64acbea7c168c5999
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-02-05 13:24:47 +0100

    mejorando estilos del home. Favicon added

--- src/app/home/components/banner/banner.component.ts
+++ src/app/home/components/banner/banner.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements OnInit {
-
+export class BannerComponent implements OnInit, AfterViewInit {
+  mySwiper: Swiper;
   images: string[] = [
-    'assets/images/banner-1.jpg',
-    'assets/images/banner-2.jpg',
-    'assets/images/banner-3.jpg',
+    'assets/images/slider-1.jpg',
+    'assets/images/slider-2.jpg',
+    'assets/images/slider-3.jpg'
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper-container', {
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+    }
+    });
+    }
 }
