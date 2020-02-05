a421514c68bc495fb94edb577cbd264dfccf6cd9
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-02-05 04:20:26 +0100

    lista la funcionalidad de subida de imágenes

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+import { finalize } from 'rxjs/operators';
+
 import { MyValidators } from './../../../utils/validators';
 
 import { ProductsService } from './../../../core/services/products/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -11,30 +16,53 @@ import { ProductsService } from './../../../core/services/products/products.serv
   styleUrls: ['./form-product.component.scss']
 })
 export class FormProductComponent implements OnInit {
-
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   saveProduct(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
-      this.productsService.createProduct(product)
-      .subscribe((newProduct) => {
+      this.productsService.createProduct(product).subscribe(newProduct => {
         console.log(newProduct);
         this.router.navigate(['./admin/products']);
       });
     }
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    console.log(file);
+
+    // console.log(file);
+    const name = file.name;
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe(url => {
+            console.log(url);
+
+            this.form.get('image').setValue(url);
+          });
+        })
+      )
+      .subscribe();
   }
 
   private buildForm() {
@@ -43,12 +71,11 @@ export class FormProductComponent implements OnInit {
       title: ['', [Validators.required]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
       image: [''],
-      description: ['', [Validators.required]],
+      description: ['', [Validators.required]]
     });
   }
 
   get priceField() {
     return this.form.get('price');
   }
-
 }
